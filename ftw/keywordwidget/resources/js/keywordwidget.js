aae6e87bcfe0773f4bf8d2b3088fdfc25898b09c
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2017-04-24 12:20:00 +0200

    Fix some selector problems after moving the js.
    Fix js

--- ftw/keywordwidget/resources/js/keywordwidget.js
+++ ftw/keywordwidget/resources/js/keywordwidget.js
@@ -30,18 +30,13 @@ $(function() {
 
         // Special template to indicate new terms
         config.templateResult = function (data) {
-          if (!data.loading) {
-            // Nested if, since we cannot use amp in a templage... grml... will change this soon.
-            if (!data._resultId) {
-                // Since this is hacked into a pt, we cannot use html tags :-(
-                return $(document.createElement('span'))
-                  .text(data.text)
-                  .addClass('newTag')
-                  .add($(document.createElement('span')).addClass('newTagHint').text(i18n.label_new));
-              } else {
-                return data.text;
-              }              
-            }
+          if (!data.loading && !data._resultId) {
+            return $('<span class="newTag" />')
+                       .text(data.text)
+                       .append($('<span class="newTagHint" />').text(i18n.label_new));
+          } else {
+            return data.text;
+          }
         };
 
         // Add and Update config for remote data
@@ -77,26 +72,24 @@ $(function() {
       if ($().select2 === undefined) {
           console.warn('You need to make sure, that select2 jquery plugin is loaded!');
       } else {
-          var widget = $('#<tal:widgetid replace="view/id" />');
-          initSelect2(widget);
+        $('.keyword-widget:visible').each(function(index, widget){
+          ftwKeywordWidgetInitSelect2($(widget));
+        });
       }
     });
 
-    $(document).on("onLoad OverlayContentReloaded", ".overlay", function() {
-      $('.keyword-widget:visible').each(function(indes, widget){
+    // select2 has issues to get right width of the placeholder element if the content is hidden and select2 gets initialized.
+    // See https://github.com/select2/select2/issues/291
+    $(document).on("click", "select.formTabs a, ul.formTabs a", function (e, index) {
+      $('.keyword-widget:visible').each(function(index, widget){
         ftwKeywordWidgetInitSelect2($(widget));
       });
+    });
 
-      // select2 has issues to get right width of the placeholder element if the content is hidden and select2 gets initialized.
-      // See https://github.com/select2/select2/issues/291
-      var tabs = $('select.formTabs, ul.formTabs');
-      tabs.on("click", function (e, index) {
-        $('.keyword-widget:visible').each(function(indes, widget){
-          ftwKeywordWidgetInitSelect2($(widget));
-        });
+    $(document).on("onLoad OverlayContentReloaded", ".overlay", function() {
+      $('.keyword-widget:visible').each(function(index, widget){
+        ftwKeywordWidgetInitSelect2($(widget));
       });
-
-
     });
 
 });
