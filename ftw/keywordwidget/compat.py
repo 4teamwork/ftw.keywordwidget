32e69e0f306a44badcf60f1889ac543bc2025d6c
Lukas Graf <lukas.graf@4teamwork.ch>
2019-11-11 17:36:01 +0100

    Fix race condition: Explicitly flush collective.indexing queue
    before building vocabularies to address some race conditions
    where catalog contents aren't up-to-date.

---
+++ ftw/keywordwidget/compat.py
@@ -0,0 +1,15 @@
+import pkg_resources
+
+
+IS_PLONE_5 = pkg_resources.get_distribution('Products.CMFPlone').version >= '5'
+
+
+if IS_PLONE_5:
+    from Products.CMFCore.indexing import processQueue
+else:
+    # optional collective.indexing support
+    try:
+        from collective.indexing.queue import processQueue
+    except ImportError:
+        def processQueue():
+            pass
