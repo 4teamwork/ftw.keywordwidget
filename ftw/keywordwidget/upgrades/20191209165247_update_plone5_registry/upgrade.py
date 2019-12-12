d82fca2a988052224a3ee09d3572e6129666d1ed
Daniel Jowett <d.jowett@4teamwork.ch>
2019-12-12 16:23:48 +0100

    Only run upgrade step for resource registry changes for Plone5 installations

--- ftw/keywordwidget/upgrades/20191209165247_update_plone5_registry/upgrade.py
+++ ftw/keywordwidget/upgrades/20191209165247_update_plone5_registry/upgrade.py
@@ -1,4 +1,7 @@
 from ftw.upgrade import UpgradeStep
+import pkg_resources
+
+IS_PLONE_5 = pkg_resources.get_distribution('Products.CMFPlone').version >= '5'
 
 
 class UpdatePlone5Registry(UpgradeStep):
@@ -6,4 +9,5 @@ class UpdatePlone5Registry(UpgradeStep):
     """
 
     def __call__(self):
-        self.install_upgrade_profile()
+        if IS_PLONE_5:
+            self.install_upgrade_profile()
